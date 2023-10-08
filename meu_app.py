4f3a8b2e1478f40878b58d3e2804de0b4510d4e7
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2023-10-08 19:38:45 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -6,7 +6,8 @@ st.set_page_config(page_title="SITE APOIO GTD DADOS B2B")
 with st.container():
     st.title("Suporte a campo SPI Dados B2B")
     st.markdown(''' 
-Formulários de Scripts:
+Formulários para gerar Scripts:
 ''')   
-    st.write("Para Gerar Script'S SWT'S [Clique aqui](https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing)")
-    st.write("Para Gerar Script's ROUTER'S [Clique aqui](https://colab.research.google.com/drive/1XXxemoXIcGC72XSWdW4pZd5AtgZYSx-H?usp=sharing)")
+    st.link_button("SWT DATACOM DM-2104", "https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing")
+    st.link_button("ROTEADORES B2B", "https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing")
+    st.link_button("CONVERSORES", "https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing")
