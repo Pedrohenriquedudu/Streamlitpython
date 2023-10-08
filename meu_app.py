6b33d201abd2fb7a6caa29a2e25f70bc237d9260
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2023-10-07 22:28:56 -0300

    Add files via upload

---
+++ meu_app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="SITE APOIO GTD DADOS B2B")
+
+with st.container():
+    st.subheader("SITE APOIO GTD DADOS B2B")
+    st.title("Formulários de Scripts")
+    st.write("RECURSOS AO TECNICO DE CAMPO SPI B2B")
+    st.write("Para Gerar Script'S SWT'S [Clique aqui](https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing)")
+    st.write("Para Gerar Script's ROUTER'S [Clique aqui](https://colab.research.google.com/drive/1XXxemoXIcGC72XSWdW4pZd5AtgZYSx-H?usp=sharing)")
+
+
