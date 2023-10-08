8d18cc32e41ac3df9fd42b04e095c7d7ad417c63
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2023-10-08 17:52:10 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -4,10 +4,9 @@ import pandas as pd
 st.set_page_config(page_title="SITE APOIO GTD DADOS B2B")
 
 with st.container():
-    st.subheader("SITE APOIO GTD DADOS B2B")
-    st.title("Formulários de Scripts")
-    st.write("RECURSOS AO TECNICO DE CAMPO SPI B2B")
+    st.title("Suporte a campo SPI Dados B2B")
+    st.markdown(''' 
+Formulários de Scripts:
+''')   
     st.write("Para Gerar Script'S SWT'S [Clique aqui](https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing)")
     st.write("Para Gerar Script's ROUTER'S [Clique aqui](https://colab.research.google.com/drive/1XXxemoXIcGC72XSWdW4pZd5AtgZYSx-H?usp=sharing)")
-    
-
