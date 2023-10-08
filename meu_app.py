3073a1566082fd4adb34c447978ea468242a57e6
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2023-10-07 23:15:18 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -9,5 +9,5 @@ with st.container():
     st.write("RECURSOS AO TECNICO DE CAMPO SPI B2B")
     st.write("Para Gerar Script'S SWT'S [Clique aqui](https://colab.research.google.com/drive/14aiImv-qA5imtWNJoP9XXvUfuc4E1I0a?usp=sharing)")
     st.write("Para Gerar Script's ROUTER'S [Clique aqui](https://colab.research.google.com/drive/1XXxemoXIcGC72XSWdW4pZd5AtgZYSx-H?usp=sharing)")
-    st.write("Para Gerar Script's ROUTER'S DMoS [Clique aqui](https://colab.research.google.com/drive/1XXxemoXIcGC72XSWdW4pZd5AtgZYSx-H?usp=sharing)")
+    
 
