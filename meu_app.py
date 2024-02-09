23ca3a2cd7f2c73c8f3529796cb5d28fa0df3162
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2024-02-08 23:38:21 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -35,31 +35,31 @@ CONSULTAS: :point_down:
 	st.link_button("Codigos Remedy","https://docs.google.com/document/d/1pUbdXcrXbND1yGss9p3s9uLjctUPSPy5/edit?usp=sharing&ouid=115435742445522021005&rtpof=true&sd=true")
            
 class Deslocamento:
-	def __init__(self):
-		self.titulo = "Deslocamento"
-		def mostrar(self):
-		 st.title(self.titulo)
-            # Adicione mais elementos à sua página inicial conforme necessário
-		 def formulario():
-			 Deslocamento = st.text_input('Deslocamento:')
-			 Técnico = st.text_input('Técnico:')
-			 Telefone = st.text_input('Telefone:')
-			 RE = st.text_input('RE:')
-			 Observação = st.text_input('Observação:')
-			 Despacho = st.text_input('Despacho:')
-			 if st.button('Gerar Carimbo'):
-				 st.success('Carimbo Gerado com Sucesso!')
-             	    		  exibir_respostas(Deslocamento,Técnico,Telefone,RE,Observação,Despacho)
-			def exibir_respostas(Deslocamento, Técnico,Telefone,RE,Observação,Despacho):
-				st.subheader('Respostas:')
-				st.write(f'Deslocamento: {Deslocamento}')
-				st.write(f'Técnico: {Técnico}')
-				st.write(f'Telefone: {Telefone}')
-				st.write(f'RE: {RE}')
-				st.write(f'Observação: {Observação}')
-				st.write(f'Despacho: {Despacho}')
-       	 		    if __name__ == '__main__':
-         		       formulario()
+def __init__(self):
+self.titulo = "Deslocamento"
+def mostrar(self):
+st.title(self.titulo)
+# Adicione mais elementos à sua página inicial conforme necessário
+def formulario():
+Deslocamento = st.text_input('Deslocamento:')
+Técnico = st.text_input('Técnico:')
+Telefone = st.text_input('Telefone:')
+RE = st.text_input('RE:')
+Observação = st.text_input('Observação:')
+Despacho = st.text_input('Despacho:')
+if st.button('Gerar Carimbo'):
+st.success('Carimbo Gerado com Sucesso!')
+	exibir_respostas(Deslocamento,Técnico,Telefone,RE,Observação,Despacho)
+def exibir_respostas(Deslocamento, Técnico,Telefone,RE,Observação,Despacho):
+st.subheader('Respostas:')
+st.write(f'Deslocamento: {Deslocamento}')
+st.write(f'Técnico: {Técnico}')
+st.write(f'Telefone: {Telefone}')
+st.write(f'RE: {RE}')
+st.write(f'Observação: {Observação}')
+st.write(f'Despacho: {Despacho}')
+       	 if __name__ == '__main__':
+         	formulario()
 
     class AberturadeHorario:
         def __init__(self):
