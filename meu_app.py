2b5f8ee05b5ce3a124e91f84af7b27eb673d0288
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2024-02-23 02:08:44 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -554,7 +554,7 @@ def main():
 
 is_authenticated = False
 
-        if "is_authenticated" in st.session_state:
+if "is_authenticated" in st.session_state:
             is_authenticated = st.session_state.is_authenticated
         
         # Se o usuário está autenticado, mostre a página protegida
