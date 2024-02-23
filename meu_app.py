7e79f86bade93c351b6e1f55955e69416b8290cb
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2024-02-23 02:47:19 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -546,10 +546,10 @@ CONSULTAS: :point_down:
         elif opcao_pagina == "Vistoria_Intragov":
             pagina = Vistoria_Intragov()
             
-            pagina.mostrar()
+        pagina.mostrar()
             
-            if __name__ == "__main__":
-                main()
+        if __name__ == "__main__":
+             main()
     
 is_authenticated = False
 
