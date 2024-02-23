9f2ce87209e64a030c6a7ec7539aaef0af843937
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2024-02-23 01:50:36 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -521,8 +521,8 @@ CONSULTAS: :point_down:
 
                 formulario()
                 
-                def main():
-                    st.sidebar.title("Navegação")
+ def main():
+    st.sidebar.title("Navegação")
     opcao_pagina = st.sidebar.radio("Escolha um Carimbo", ["Principal","Deslocamento", "Abertura de Horario","Notificação","Baixa_DDR","Ativação_Gpon_Router","Ativação_Dslan_V35","Ativação_SWT_Router","Ativação_Conversor","Vistoria_Intragov"])
     
     if opcao_pagina == "Principal":
