4f37e88144c42c4a7d74549d86f3e67cc1e16bbc
Pedrohenriquedudu <147287312+Pedrohenriquedudu@users.noreply.github.com>
2024-02-23 01:02:17 -0300

    Update meu_app.py

--- meu_app.py
+++ meu_app.py
@@ -487,7 +487,9 @@ CONSULTAS: :point_down:
                 st.write(f'Analista_ESSE: {Analista_ESSE}')
                 
         if __name__ == '__main__':
+            
             formulario()
+
     
     class Ativação_Conversor:
         def __init__(self):
